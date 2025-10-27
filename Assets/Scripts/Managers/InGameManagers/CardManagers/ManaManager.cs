61821d3da1820e786bd7e7a68beec3004b78682e
Tunahan0707 <tnhbykkacar07antalya@gmail.com>
2025-10-27 22:30:09 +0300

    Update ManaManager.cs

--- Assets/Scripts/Managers/InGameManagers/CardManagers/ManaManager.cs
+++ Assets/Scripts/Managers/InGameManagers/CardManagers/ManaManager.cs
@@ -9,7 +9,6 @@ public class ManaManager : MonoBehaviour
     public static int maxMana;
     [SerializeField] private PlayersDataBase pDB;
     [SerializeField] private TextMeshProUGUI manaText;
-    [SerializeField] private PlayersDataBase pDB;
     private static TextMeshProUGUI ManaText;
     private FightData loadedData => FightDataHolder.Instance.fightData;
     private PlayerData playerData => PlayerDataHolder.Instance.playerData;
